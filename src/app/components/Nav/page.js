5254a612ed07d7ce80172c8aacbac67f5b9a844e
William Kosso <wkosso@iu.edu>
2024-11-12 21:33:04 -0500

    Styled the home main content. and changed the navigation names

--- src/app/components/Nav/page.js
+++ src/app/components/Nav/page.js
@@ -3,8 +3,8 @@ export default function Nav() {
     return (
         <nav className={navStyles.nav}>
             <a href="/">Home</a>
-            <a href="/about">About</a>
-            <a href="/explore">Explore</a>
+            <a href="/about">About Cactus Wildlife</a>
+            <a href="/explore">Cactus Explore</a>
         </nav>
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,21 @@ export default function Home() {
       <div className={PageStyles.homeBanner}>
         <h1 className={PageStyles.homeBannerTitle}>Saguaro Wildlife Park</h1>
       </div>
+
+
+      <div className={PageStyles.homecontentContainer}>
+        <div className={PageStyles.homecontentText}>Saguaro National Park protects our largest cacti species, the saguaro, and features camping, hiking, and backpacking trails for every level.
+       For many, the Giant Saguaro cactus, silhouetted by the setting sun, is the universal symbol of the American West. And yet, these majestic plants are only found in a small portion of the United States. On the edge of the modern City of Tucson, Saguaro Park protects some of the most impressive forests of these sub-tropical giants, which can live for 200 years and grow to 50 feet tall.
+        </div>
+
+        <div className={PageStyles.planboxContainer} >
+
+          <div>Planning a Visit?
+          For information about the park, visit the National Park Service website for Saguaro National Park:</div>
+
+          <div className={PageStyles.planbutton}>Learn More</div>
+        </div>
+      </div>
     </main>
   );
 }
